33e1629e5cadad58cd8fe34982555dccb2324ac9
Jan Jorissen <grasmaaier45@gmail.com>
2020-12-05 16:11:44 +0100

    update: easier config

--- index.js
+++ index.js
@@ -4,51 +4,114 @@ const CLI = require('clui'),
     clear = require('clear');
 const fetch = require('node-fetch');
 const numbro = require('numbro');
+const {Table} = require('console-table-printer');
+
+let num = 0, STATE = {info: {}, historical: {}};
+
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const holdings = {
-    'havven': {
-        name: 'SNX',
-        amount: 1000
-    },
-    'ethereum': {
-        name: 'ETH',
-        amount: 100
-    },
-    'bitcoin': {
-        name: 'BTC',
-        amount: 10
-    },
-    'yearn-finance': {
-        name: 'YFI',
-        amount: 1
-    }
+const setState = (state) => {
+    STATE = {...STATE, ...state};
 }
 
-const updateUI = async (data) => {
-    if(!data) return;
-    clear();
+const loader = () => {
+    num++;
+    const spinner = ['.', '..', '...'];
+    return spinner[num % 3]
 }
-const main = async () => {
+
+
+const holdings = {
+    'havven': 1000,
+    'ethereum': 100,
+    'bitcoin': 10,
+    'yearn-finance': 1,
+}
+
+const updateUI = async () => {
+    if (STATE.prices) {
+        clear();
+        const load = loader();
+        const table = new Table({
+            columns: [
+                {name: 'name', alignment: 'left', color: 'yellow', title: 'Name'}, //with alignment and color
+                {name: 'ticker', alignment: 'right', title: 'Ticker'},
+                {name: 'price', title: 'Price'},
+                {name: 'eth', title: 'ETH Price'},
+                {name: 'holdings', title: 'Holdings'},
+                {name: 'total', title: 'Total'},
+            ],
+        });
+
+        let total = 0;
+        Object.entries(holdings).forEach(([id, value]) => {
+            total += (value * STATE.prices[id]);
+            table.addRow({
+                price: STATE.prices[id] ? numbro(STATE.prices[id]).format({
+                    mantissa: 1,
+                    thousandSeparated: true
+                }) : load,
+                eth: STATE.prices[id] ? numbro(STATE.prices[id] / STATE.prices.ethereum).format({
+                    mantissa: 5,
+                    thousandSeparated: true
+                }) : load,
+                name: STATE.info[id] ? STATE.info[id].name : load,
+                ticker: STATE.info[id] ? STATE.info[id].symbol.toLocaleUpperCase() : load,
+                holdings: numbro(value).format({thousandSeparated: true}),
+                total: STATE.prices[id] ? numbro(value * STATE.prices[id]).formatCurrency({
+                    mantissa: 1,
+                    thousandSeparated: true
+                }) : load,
+            })
+        })
+        table.printTable()
+        console.log(' \n')
+        console.log(`  Portfolio value: ${numbro(total).formatCurrency({mantissa: 1, thousandSeparated: true})}`)
+        console.log(' \n')
+        console.log(loader());
+        console.log('')
+    }
+    await sleep(250);
+    updateUI();
+}
+
+const priceFetcher = async () => {
     const ids = Object.keys(holdings).join('%2C');
     const data = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=usd`);
     const json = await data.json();
-    updateUI(json);
-    const result = Object.entries(json).map(([key, { usd: price }]) => {
-        const { name, amount } = holdings[key];
-        return {
-            Token: name,
-            Price: price,
-            Holdings: numbro(amount).format(),
-            Total: numbro(amount * price).formatCurrency(),
-            sum: amount * price,
-        }
+    const result = {};
+    Object.entries(json).map(([key, {usd: price}]) => {
+        result[key] = price;
     })
-    console.table(result);
-    console.log('')
-    console.log(`  Portfolio Value: ${numbro(_.sumBy(result, 'sum')).formatCurrency()}`)
+    setState({prices: result})
     await sleep(5000);
-    main();
+    priceFetcher();
 }
 
-main();
+const informationFetcher = () => {
+    const ids = Object.keys(holdings);
+    ids.forEach(async (id, i) => {
+        await sleep(i * 300);
+        const data = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`);
+        const json = await data.json();
+        const info = STATE.info || {};
+        setState({info: {[id]: json, ...info}});
+    })
+}
+
+const historicalFetcher = async () => {
+    await sleep(1000);
+    const ids = Object.keys(holdings);
+    ids.map(async (id, i) => {
+        await sleep(i * 300);
+        const data = await fetch(`https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=30&interval=daily`);
+        const json = await data.json();
+        const info = STATE.historical || {};
+        setState({historical: {[id]: json, ...info}});
+    });
+}
+
+priceFetcher();
+informationFetcher();
+historicalFetcher();
+updateUI();
